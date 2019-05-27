03c83612b92f6a7ec8c15e233f33b5baaa80affd
Kunal-Nare <Kunal-Nare@users.noreply.github.com>
2019-05-28 01:16:20 +0530

    Code file created.

---
+++ Basic-Pandas/code.py
@@ -0,0 +1,85 @@
+# --------------
+# Import packages
+import numpy as np
+import pandas as pd
+from scipy.stats import mode 
+
+# code starts here
+bank = pd.read_csv(path)
+categorical_var = bank.select_dtypes(include = 'object')
+print(categorical_var)
+numerical_var = bank.select_dtypes(include = 'number')
+print(numerical_var)
+
+
+# code ends here
+
+
+# --------------
+# code starts here
+
+banks = bank.drop(["Loan_ID"],1)
+
+print(banks.isnull().sum())
+
+bank_mode = banks.mode()
+
+for col_name in banks.columns :
+    banks[col_name].fillna(banks[col_name].mode()[0], inplace=True)
+
+print(banks)
+
+
+#code ends here
+
+
+# --------------
+# Code starts here
+
+avg_loan_amount = pd.pivot_table(banks, index=['Gender','Married','Self_Employed'],values='LoanAmount')
+
+print(avg_loan_amount)
+
+
+# code ends here
+
+
+
+# --------------
+# code starts here
+
+loan_approved_se = banks[(banks['Self_Employed']=='Yes') & (banks['Loan_Status']=='Y')].shape[0]
+
+loan_approved_nse = banks[(banks['Self_Employed']=='No') & (banks['Loan_Status']=='Y')].shape[0]
+
+Loan_Status = 614
+
+percentage_se = (loan_approved_se/Loan_Status)*100
+
+percentage_nse = (loan_approved_nse/Loan_Status)*100
+# code ends here
+
+
+# --------------
+# code starts here
+
+loan_term = banks['Loan_Amount_Term'].apply(lambda x : (x/12))
+
+big_loan_term = (pd.DataFrame(loan_term)[pd.DataFrame(loan_term)['Loan_Amount_Term']>=25]).shape[0]
+
+# code ends here
+
+
+
+# --------------
+# code starts here
+
+loan_groupby = banks.groupby('Loan_Status')
+
+loan_groupby = loan_groupby['ApplicantIncome','Credit_History']
+
+mean_values = loan_groupby.mean()
+
+# code ends here
+
+
